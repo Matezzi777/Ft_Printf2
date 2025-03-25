88ab02d7cd7e032fb560addc09a908f1a371dd8a
Matezzi777 <matezzi777@gmail.com>
2025-03-26 00:47:48 +0100

    Project restart from scratch

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 03:37:11 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/03/11 05:03:36 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/03/26 00:46:11 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,10 @@
 
 // static int	print_result(const char *format, t_list *params);
 
-int	ft_printf(const char *format, ...)
+int	ft_printf(const char *str)
 {
-	va_list		arglist;
-	t_list		*params;
-
-	va_start(arglist, format);
-	params = parse_arguments(format, arglist);
-
-	return (test_parsing(params, arglist));
-
-	// if (!params)
-	// 	return (clean_exit(0, params, arglist));
-	// return (clean_exit(print_result(format, params), params, arglist));
+	ft_putendl_fd((char *)str, 1);
+	return (1);
 }
 
 // static int	print_result(const char *format, t_list *params)
--- test.c
+++ test.c
@@ -6,11 +6,8 @@ int	main(int argc, char **argv)
 	int	i;
 
 	if (argc < 2)
-		return (printf("Not enough params.\n"));
+		return (ft_printf("Not enough args."));
 	i = 0;
 	while (++i < argc)
-	{
-		ft_putstr_fd("\n\n\n", 1);
 		ft_printf(argv[i]);
-	}
 }
