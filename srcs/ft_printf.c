6aa2a896ac5e25b4eceb86f021441b9028b5e309
Matezzi777 <matezzi777@gmail.com>
2025-04-09 00:49:37 +0200

    rebasing ?

--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,31 +6,10 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 03:37:11 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/03/11 05:03:36 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/03/25 23:15:36 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-// static int	print_result(const char *format, t_list *params);
-
-int	ft_printf(const char *format, ...)
-{
-	va_list		arglist;
-	t_list		*params;
-
-	va_start(arglist, format);
-	params = parse_arguments(format, arglist);
-
-	return (test_parsing(params, arglist));
-
-	// if (!params)
-	// 	return (clean_exit(0, params, arglist));
-	// return (clean_exit(print_result(format, params), params, arglist));
-}
-
-// static int	print_result(const char *format, t_list *params)
-// {
-	
-// }
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,35 +6,10 @@
 /*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/11 03:54:28 by maxmart2          #+#    #+#             */
-/*   Updated: 2025/03/12 23:28:53 by maxmart2         ###   ########.fr       */
+/*   Updated: 2025/03/25 23:15:42 by maxmart2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 #include "libft.h"
 
-t_list	*parse_arguments(const char *format, va_list arglist)
-{
-	t_list	*params;
-	int		i;
-
-	params = NULL;
-	i = 0;
-	while (format[i])
-	{
-		while (format[i] != '%')
-			i++;
-		if (format[i])
-		{
-			if (!add_param(format, &i, params, arglist))
-				return ((t_list *)clean_exit(0, params, arglist));
-		}
-	}
-	return (params);
-}
-
-int	test_parsing(t_list *params, va_list arglist)
-{
-	printf("Arguments found : %d\n", ft_lstsize(params));
-	return (clean_exit(0, params, arglist));
-}
