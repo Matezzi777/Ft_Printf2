07334320b78c6b9555133ab2be0da22abc3d8413
Matezzi777 <matezzi777@gmail.com>
2025-04-11 17:01:23 +0200

    Project updated

---
+++ includes/ft_printf.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: maxmart2 <maxmart2@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/26 03:30:01 by maxmart2          #+#    #+#             */
+/*   Updated: 2025/04/11 15:55:07 by maxmart2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include "libft.h"
+# include <stdarg.h>
+# include <stdint.h>
+
+typedef enum e_bool
+{
+	FALSE,
+	TRUE
+}				t_bool;
+
+//ft_printf.c
+int		ft_printf(const char *format, ...);
+int		ft_print_character(int c);
+int		ft_print_string(char *str);
+int		ft_print_hexa(int nbr, t_bool maj);
+int		ft_print_integer(int nbr);
+int		ft_print_unsigned(unsigned int nbr);
+int		ft_print_address(void *addr);
+
+#endif
